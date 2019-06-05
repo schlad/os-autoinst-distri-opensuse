b35852c352fad2b6d3d80d44557ff63426f9dc2d
Sebastian Chlad <sebastian.chlad@gmail.com>
2019-06-05 23:29:46 +0200

    Just an idea

---
+++ tests/hpc/slurm_master_alpha.pm
@@ -0,0 +1,62 @@
+# SUSE's openQA tests
+#
+# Copyright © 2017-2019 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Summary: HPC_Module: slurm failover - database
+#    This test is setting up slurm controle nodes with accounting configured
+#    (database) so that the failover of the ctls can be tested
+# Maintainer: Sebastian Chlad <sebastian.chlad@suse.com>
+
+use base "hpcbase";
+use strict;
+use warnings;
+use testapi;
+use lockapi;
+use utils;
+
+sub run {
+    my $self = shift;
+    $self->prepare_user_and_group();
+
+    # provision HPC cluster, so the proper rpms are installed,
+    # munge key is distributed to all nodes, so is slurm.conf
+    # and proper services are enabled and started
+    zypper_call('in slurm slurm-munge');
+    $self->prepare_slurm_conf();
+    barrier_wait("SLURM_SETUP_DONE");
+    $self->distribute_munge_key();
+    $slef->distribute_slurm_conf();
+    $self->enable_and_start('munge');
+    $self->enable_and_start('slurmctld');
+    systemctl 'status slurmctld';
+    $self->enable_and_start('slurmd');
+    systemctl 'status slurmd';
+
+    # wait for slave to be ready
+    barrier_wait("SLURM_MASTER_SERVICE_ENABLED");
+    barrier_wait("SLURM_SLAVE_SERVICE_ENABLED");
+
+    # run the actual test against both nodes
+    assert_script_run("srun -N ${nodes} /bin/ls");
+
+    barrier_wait('SLURM_MASTER_RUN_TESTS');
+}
+
+sub test_flags {
+    return {fatal => 1, milestone => 1};
+}
+
+sub post_fail_hook {
+    my ($self) = @_;
+    $self->select_serial_terminal;
+    $self->upload_service_log('slurmd');
+    $self->upload_service_log('munge');
+    $self->upload_service_log('slurmctld');
+}
+
+1;
